0ef77e0c65e5478109ed7ca103cc3c211dbd258b
Danil <thrixstudio@yandex.ru>
2022-06-19 15:07:28 +0700

    main: ASync cancelled...

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from telebot import types
 from telebot.async_telebot import AsyncTeleBot
-import asyncio
+from telebot import TeleBot
 import config
 import reghandler as reg
 import dbhandler as db
@@ -8,7 +8,7 @@ import dbhandler as db
 # Owner info
 owner = config.OWNER
 
-bot = AsyncTeleBot(config.TOKEN, parse_mode=config.PARSE_MODE)
+bot = TeleBot(config.TOKEN, parse_mode=config.PARSE_MODE)
 
 # Commands list and their descriptions
 commands = [
@@ -46,10 +46,10 @@ for button in default_keyboard_list:
 
 
 @bot.message_handler(commands=["start"])
-async def start_handler(message: types.Message):
+def start_handler(message: types.Message):
     sticker = open("../assets/Hello.webp", "rb")
-    await bot.send_sticker(message.chat.id, sticker)
-    await bot.send_message(
+    bot.send_sticker(message.chat.id, sticker)
+    bot.send_message(
         message.chat.id,
         f"Привет, **{message.from_user.first_name}**!\n"
         + "Вот список доступных команд:\n\n"
@@ -59,33 +59,31 @@ async def start_handler(message: types.Message):
 
 
 @bot.message_handler(commands=["help"])
-async def help_handler(message: types.Message):
-    await bot.send_message(message.chat.id, help_message)
+def help_handler(message: types.Message):
+    bot.send_message(message.chat.id, help_message)
 
 
 @bot.message_handler(commands=["link"])
-async def url_handler(message: types.Message):
+def url_handler(message: types.Message):
     markup = types.InlineKeyboardMarkup(row_width=1)
     write_me_button = types.InlineKeyboardButton(
         "Платформа школы 21", "https://edu.21-school.ru/"
     )
     markup.add(write_me_button)
-    await bot.send_message(
-        message.chat.id, "https://edu.21-school.ru/", reply_markup=markup
-    )
+    bot.send_message(message.chat.id, "https://edu.21-school.ru/", reply_markup=markup)
 
 
 @bot.message_handler(commands=["register"])
-async def registration_handler(message: types.Message):
+def registration_handler(message: types.Message):
     if not db.read_from_db(str(message.chat.id)):
         user_info[str(message.chat.id)] = ["Junk"]
-        await bot.send_message(
+        bot.send_message(
             message.chat.id,
             "Введи свой **логин** на платформе",
             reply_markup=reg.cancel_markup_inline,
         )
     else:
-        await bot.send_message(
+        bot.send_message(
             message.chat.id,
             "Похоже, ты уже зарегистрирован в системе отслеживания оповещений.\nПопробуй выполнить команду /unregister и попробовать ещё раз.\n\n"
             + f"Если это не помогло, обратись к моему Создателю: @{config.OWNER}",
@@ -93,14 +91,14 @@ async def registration_handler(message: types.Message):
 
 
 @bot.message_handler(commands=["unregister"])
-async def unreg_handler(message: types.Message):
+def unreg_handler(message: types.Message):
     if db.read_from_db(str(message.chat.id)):
         db.remove_from_db(str(message.chat.id))
-        await bot.send_message(
+        bot.send_message(
             message.chat.id, "Твой никнейм успешно удалён из истемы отслеживания."
         )
     else:
-        await bot.send_message(
+        bot.send_message(
             message.chat.id,
             "Похоже, ты ещё не зарегистрирован в системе отслеживания оповещений.\n\n"
             + f"Сперва зарегистрируйся, введя команду /register",
@@ -108,22 +106,22 @@ async def unreg_handler(message: types.Message):
 
 
 @bot.message_handler(commands=["check"])
-async def check_registration_handler(message: types.Message):
+def check_registration_handler(message: types.Message):
     if db.read_from_db(str(message.chat.id)):
-        await bot.send_message(
+        bot.send_message(
             message.chat.id, "Твой id уже зарегистрирован в системе отслеживания."
         )
     else:
-        await bot.send_message(
+        bot.send_message(
             message.chat.id, "Твой id ещё не зарегистрирован в системе отслеживания."
         )
 
 
-async def handle_unknown(message: types.Message):
+def handle_unknown(message: types.Message):
     markup = types.InlineKeyboardMarkup(row_width=1)
     write_me_button = types.InlineKeyboardButton("Напиши!", "telegram.me/" + owner)
     markup.add(write_me_button)
-    await bot.send_message(
+    bot.send_message(
         message.chat.id,
         "Такой команды я не знаю. Возможно, мой Создатель её ещё не реализовал...\n\n"
         + f"Если хочешь, чтобы Создатель реализовал эту команду, **напиши** ему: @{owner}",
@@ -137,8 +135,8 @@ def check_help_text(message: types.Message):
 
 
 @bot.message_handler(content_types=["text"], func=check_help_text)
-async def reg_text_handler(message: types.Message):
-    await help_handler(message)
+def reg_text_handler(message: types.Message):
+    help_handler(message)
 
 
 def check_platform_text(message: types.Message):
@@ -147,8 +145,8 @@ def check_platform_text(message: types.Message):
 
 
 @bot.message_handler(content_types=["text"], func=check_platform_text)
-async def platform_text_handler(message: types.Message):
-    await url_handler(message)
+def platform_text_handler(message: types.Message):
+    url_handler(message)
 
 
 def check_reg_text(message: types.Message):
@@ -157,16 +155,16 @@ def check_reg_text(message: types.Message):
 
 
 @bot.message_handler(content_types=["text"], func=check_reg_text)
-async def reg_text_handler(message: types.Message):
-    await registration_handler(message)
+def reg_text_handler(message: types.Message):
+    registration_handler(message)
 
 
 @bot.message_handler(content_types=["text"])
-async def text_handler(message: types.Message):
+def text_handler(message: types.Message):
     if user_info.get(str(message.chat.id), None) is not None:
         if len(user_info.get(str(message.chat.id))) == 1:
             user_info.get(str(message.chat.id)).append(message.text)
-            await bot.send_message(
+            bot.send_message(
                 message.chat.id,
                 "Теперь введи **пароль** от платформы\n\n__Не бойся, я шифрую данные, так что не смогу их применить__",
                 reply_markup=reg.cancel_markup_inline,
@@ -178,7 +176,7 @@ async def text_handler(message: types.Message):
                 user_info.get(str(message.chat.id))[1],
                 (str(message.chat.id)),
             )
-            await bot.send_message(
+            bot.send_message(
                 message.chat.id,
                 f"Ты зарегистрировался под ником **{user_info.get(str(message.chat.id))[1]}**\n\nДля отмены регистрации выполни команду /unregister"
                 + "\n**ОБЯЗАТЕЛЬНО** удали сообщение с паролем в целях конфиденциальности!",
@@ -186,19 +184,19 @@ async def text_handler(message: types.Message):
             user_info.pop(str(message.chat.id), [])
         else:
             user_info.pop(str(message.chat.id), [])
-            await bot.send_message(
+            bot.send_message(
                 message.chat.id,
                 "Что-то пошло не так. Попробуй ещё раз",
                 reply_markup=None,
             )
     else:
-        await handle_unknown(message)
+        handle_unknown(message)
 
 
 @bot.callback_query_handler(func=lambda call: True)
-async def inline_buttons_handler(call: types.CallbackQuery):
+def inline_buttons_handler(call: types.CallbackQuery):
     if call.data == "cancel":
-        await bot.edit_message_text(
+        bot.edit_message_text(
             call.message.text + "\n\n__Действие отменено__",
             call.message.chat.id,
             call.message.id,
@@ -207,9 +205,9 @@ async def inline_buttons_handler(call: types.CallbackQuery):
         user_info.pop(call.message.chat.id)
 
 
-async def main():
-    await bot.infinity_polling()
+def main():
+    bot.infinity_polling()
 
 
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    main()
