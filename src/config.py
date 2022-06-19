e44c3e853b703e8e09eb1ca2d8545d9e89c9c789
Danil <thrixstudio@yandex.ru>
2022-06-19 14:17:06 +0700

    main: Deleting config

--- src/config.py
+++
@@ -1,4 +0,0 @@
-TOKEN = "5598590455:AAE7KWLxOymGYoN0panqe07fMsloByWtuus"
-OWNER = "astadasta"
-PARSE_MODE = "markdown"
-SECRET_WORD = "hi7t7guyvrbnkwf367i8oiuoipSomevmkeonwesiobiFancye12t54ti5j89354gt5VFjvWordnjksNNjkNJb"
