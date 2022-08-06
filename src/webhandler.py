561e68c59ef15a71ad902767b12afcde4132e4ef
Danil <thrixstudio@yandex.ru>
2022-08-06 22:26:17 +0700

    develop: decomposed code into functions

--- src/webhandler.py
+++ src/webhandler.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from time import sleep
-from bs4 import BeautifulSoup as bs
+from bs4 import BeautifulSoup as bs, ResultSet
 from config import CHROME_LOCATION
 
 # Variables for searching for notifications
@@ -21,29 +21,23 @@ def auth_into_platform(username: str, password: str, driver: webdriver.Chrome = 
         driver = webdriver.Chrome(executable_path="chromedriver", options=op)
     if driver.current_url != link:
         driver.get(link)  # link = "https://edu.21-school.ru"
+        print(f'- Trying to authorizate user "{username}"...')
         elem = driver.find_element(by=By.NAME, value="username")
         elem.send_keys(f"{username}@student.21-school.ru")
         elem = driver.find_element(by=By.NAME, value="password")
         elem.send_keys(password)
         elem.send_keys(Keys.ENTER)
-        print(f'- Trying to authorizate user "{username}"...')
         sleep(10)
         if "auth.sberclass.ru" in driver.current_url:
             driver.close()
             print("- Authorization failed!")
             return None
-        print("- Authorization success!")
-        return driver
+        else:
+            print("- Authorization success!")
+            return driver
 
 
-# Function to get list of planned participant peer reviews
-def get_ppr_list(driver: webdriver.Chrome):
-    driver.get(calendar)
-    # Need to somehow check if page loaded fully and successfully
-    print(f"Getting calendar page data...")
-    sleep(10)
-    source_code = driver.page_source
-    driver.close()
+def find_needed_element(source_code: str):
     content = bs(source_code, "lxml")
     current_date = (
         content.find("div")
@@ -73,33 +67,49 @@ def get_ppr_list(driver: webdriver.Chrome):
         .find_next_sibling("div")
         .find_all("div")[count]
     )
+    return current_date_column
+
+
+def parse_list(current_date_column: ResultSet):
+    events_list = list(dict())
+    for event in current_date_column.children:
+        style_str = event.get("style").split(";")
+        height = int(style_str[1][9:-2]) // 14
+        events_list.append(
+            {
+                "end_time": "garbage",
+                "start_time": event.find_next("div").text,
+                "event": event.find_next("div").find_next("div").text,
+            }
+        )
+        start_time = events_list[-1]["start_time"].split(":")
+        start_time = int(start_time[0]) * 60 + int(start_time[1])
+        end_time = start_time + height * 15
+        hour, min = end_time // 60, end_time % 60
+        hour = str(hour)
+        if len(hour) < 2:
+            hour = "0" + hour
+        min = str(min)
+        if len(min) == 1:
+            min += "0"
+        end_time = hour + ":" + min
+        events_list[-1]["end_time"] = end_time
+        return events_list
+
+
+# Function to get list of planned participant peer reviews
+def get_ppr_list(driver: webdriver.Chrome):
+    driver.get(calendar)
+    # Need to somehow check if page loaded fully and successfully
+    print(f"Getting calendar page data...")
+    sleep(10)
+    source_code = driver.page_source
+    driver.close()
+    current_date_column = find_needed_element(source_code)
     if len(current_date_column) == 0:
         return None
     else:
-        events_list = list(dict())
-        for event in current_date_column.children:
-            style_str = event.get("style").split(";")
-            height = int(style_str[1][9:-2]) // 14
-            events_list.append(
-                {
-                    "end_time": "garbage",
-                    "start_time": event.find_next("div").text,
-                    "event": event.find_next("div").find_next("div").text,
-                }
-            )
-            start_time = events_list[-1]["start_time"].split(":")
-            start_time = int(start_time[0]) * 60 + int(start_time[1])
-            end_time = start_time + height * 15
-            hour, min = end_time // 60, end_time % 60
-            hour = str(hour)
-            if len(hour) < 2:
-                hour = "0" + hour
-            min = str(min)
-            if len(min) == 1:
-                min += "0"
-            end_time = hour + ":" + min
-            events_list[-1]["end_time"] = end_time
-        return events_list
+        return parse_list(current_date_column)
 
 
 def get_today_events(username: str, password: str):
